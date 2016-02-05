0f7b1df95ab5b390b194e52e75e9ea4a11921e0f
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 17:08:45 -0500

    New Commit, Talk to ben about it

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -10,31 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBAction func numberPressed1(sender: UIButton) {
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func btnClicked(sender: UIButton) {
+        if (display.text == "0")
+        {
+            display.text = sender.currentTitle
+        }
+        display.text = display.text! + sender.currentTitle!
+    }
+    @IBAction func equalsClicked(sender: UIButton) {
         
     }
     
-    @IBAction func calculationPressedMinus(sender: UIButton) {
-    }
-    @IBAction func calculationPressedMultiply(sender: UIButton) {
-    }
-    @IBAction func calculationPressedDivide(sender: UIButton) {
+    @IBAction func calculationClicked(sender: UIButton) {
+        
     }
     
-    @IBAction func calculationPressedPlus(sender: UIButton) {
-    }
-    @IBOutlet weak var calculatorDisplay: UILabel!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
