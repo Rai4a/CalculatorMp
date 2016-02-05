c79930e0dc8d97923d2fd2f149de2c1b4a9504e0
student <student@students-mac.student.georgianc.on.ca>
2016-02-05 12:02:07 -0500

    test

--- calculatorApp/ViewController.swift
+++ calculatorApp/ViewController.swift
@@ -9,6 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func numberPressed1(sender: UIButton) {
+        
+    }
+    @IBAction func numberPressed2(sender: UIButton) {
+    }
+    @IBAction func numberPressed3(sender: UIButton) {
+    }
+    @IBAction func numberPressed4(sender: UIButton) {
+    }
+    @IBAction func numberPressed5(sender: UIButton) {
+    }
+    @IBAction func numberPressed6(sender: UIButton) {
+    }
+    @IBAction func numberPressed7(sender: UIButton) {
+    }
+    @IBAction func numberPressed8(sender: UIButton) {
+    }
+    @IBAction func numberPressed9(sender: UIButton) {
+    }
+    @IBAction func numberPressed0(sender: UIButton) {
+    }
+    @IBAction func calculationPressedMinus(sender: UIButton) {
+    }
+    @IBAction func calculationPressedMultiply(sender: UIButton) {
+    }
+    @IBAction func calculationPressedDivide(sender: UIButton) {
+    }
+    
+    @IBAction func calculationPressedPlus(sender: UIButton) {
+    }
+    @IBOutlet weak var calculatorDisplay: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
